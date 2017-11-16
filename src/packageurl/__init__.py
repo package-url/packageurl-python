c12f9c1d095ae0e2c4f4c52159d50c270098e87b
Philippe Ombredanne <pombredanne@nexb.com>
2017-11-16 19:54:03 +0100

    Extract fields normalization in a single normalize() function
     * normalize all the components in and out
     * either quote or unquote as needed
     * ensure that % are not percent-encoded
     * simplify vastly from_string and to_string methods
     * remove requireved version check
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- src/packageurl/__init__.py
+++ src/packageurl/__init__.py
@@ -25,9 +25,8 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from collections import namedtuple
-from urllib import quote
+from urllib import quote as url_quote
 from urlparse import unquote
-
 
 # Python 2 and 3 support
 try:
@@ -47,16 +46,100 @@ https://github.com/package-url/purl-spec
 """
 
 
-class PackageURL(namedtuple('PackageURL',
-    ['type', 'namespace', 'name', 'version', 'qualifiers', 'subpath'])):
+def quote(s):
+    """
+    Percent-encode a string, except for colon :
+    """
+    quoted = url_quote(s)
+    return quoted.replace('%3A', ':')
+
+
+def normalize(type, namespace, name, version, qualifiers, subpath, encode=True):
+    """
+    Return normalized purl components.
+    """
+    if encode is True:
+        quoting = quote
+    elif encode is False:
+        quoting = unquote
+    elif encode is None:
+        quoting = lambda x: x
+
+    if type:
+        type = type.strip().lower()
+
+    if namespace:
+        namespace = namespace.strip().strip('/')
+        if type and type in ('bitbucket', 'github', 'pypi'):
+            namespace = namespace.lower()
+        segments = namespace.split('/')
+        segments = [seg for seg in segments if seg and seg.strip()]
+        segments = map(quoting, segments)
+        namespace = '/'.join(segments)
+
+    if name:
+        name = name.strip().strip('/')
+        if type in ('bitbucket', 'github', 'pypi',):
+            name = name.lower()
+        if type in ('pypi',):
+            name = name.replace('_', '-')
+        name = quoting(name)
+
+    name = name or None
+
+    if version:
+        version = quoting(version.strip())
+
+    if qualifiers:
+        if isinstance(qualifiers, basestring):
+            # decode string to dict
+            qualifiers = qualifiers.split('&')
+            qualifiers = [kv.partition('=') for kv in qualifiers]
+            if qualifiers:
+                qualifiers = [(k, v) for k, _, v in qualifiers]
+            else:
+                qualifiers = []
+        elif isinstance(qualifiers, dict):
+            qualifiers = qualifiers.items()
+        else:
+            raise ValueError(
+                'Invalid qualifier. '
+                'Must be a string or dict:{}'.format(repr(qualifiers)))
+
+        if qualifiers:
+            qualifiers = {
+                k.strip().lower(): quoting(v)
+                for k, v in qualifiers
+                if k and k.strip() and v and v.strip()
+            }
+
+            if qualifiers and encode is True:
+                # encode dict as a string
+                qualifiers = sorted(qualifiers.items())
+                qualifiers = ['{}={}'.format(k, v) for k, v in qualifiers]
+                qualifiers = '&'.join(qualifiers)
+
+    if subpath:
+        segments = subpath.split('/')
+        segments = [quoting(s) for s in segments if s and s.strip()
+                    and s not in ('.', '..')]
+        subpath = '/'.join(segments)
+
+    return (type or None, namespace or None, name or None, version or None,
+            qualifiers or None, subpath or None)
+
+
+_components = ['type', 'namespace', 'name', 'version', 'qualifiers', 'subpath']
+class PackageURL(namedtuple('PackageURL', _components)):
     """
     A purl is a package URL as defined at
-    https://github.com/package-url/purl-spec.
+    https://github.com/package-url/purl-spec
     """
+
     def __new__(self, type=None, namespace=None, name=None,
                 version=None, qualifiers=None, subpath=None):
 
-        required = dict(type=type, namespace=namespace, name=name)
+        required = dict(type=type, name=name)
         for key, value in required.items():
             if value:
                 continue
@@ -66,18 +149,21 @@ class PackageURL(namedtuple('PackageURL',
         strings = dict(type=type, namespace=namespace, name=name,
                        version=version, subpath=subpath)
         for key, value in strings.items():
-            if not value or isinstance(value, basestring):
+            if value and isinstance(value, basestring) or not value:
                 continue
-            raise ValueError('Invalid purl: {} argument must be a string: {}'
+            raise ValueError('Invalid purl: {} argument must be a string: {}.'
                              .format(key, repr(value)))
 
         if qualifiers and not isinstance(qualifiers, dict):
-            raise ValueError('Invalid purl: {} argument must be a dict: {}'
+            raise ValueError('Invalid purl: {} argument must be a dict: {}.'
                              .format('qualifiers', repr(qualifiers)))
 
+        type, namespace, name, version, qualifiers, subpath = normalize(
+            type, namespace, name, version, qualifiers, subpath, encode=None)
+
         return super(PackageURL, self).__new__(PackageURL, type=type,
-            namespace=namespace or None, name=name, version=version or None,
-            qualifiers=qualifiers or None, subpath=subpath or None)
+            namespace=namespace, name=name, version=version,
+            qualifiers=qualifiers, subpath=subpath)
 
     def __str__(self, *args, **kwargs):
         return self.to_string()
@@ -92,64 +178,39 @@ class PackageURL(namedtuple('PackageURL',
         """
         Return a purl string built from components.
         """
-        purl = []
-        if self.type:
-            purl.append(self.type.strip().lower())
-            purl.append(':')
+        type, namespace, name, version, qualifiers, subpath = normalize(
+            self.type, self.namespace, self.name, self.version,
+            self.qualifiers, self.subpath,
+            encode=True
+        )
 
-        if self.namespace:
-            namespace = self.namespace.strip().strip('/')
-            if self.type and self.type in ('bitbucket', 'github',):
-                namespace = namespace.lower()
-            segments = namespace.split('/')
-            segments = [seg for seg in segments if seg and seg.strip()]
-            if segments:
-                subpaths = map(quote, segments)
-                purl.append('/'.join(segments))
-                purl.append('/')
+        purl = [type, ':']
 
-        name = self.name.strip().strip('/')
-        if self.type and self.type in ('bitbucket', 'github', 'pypi',):
-            name = name.lower()
-        if self.type and self.type in ('pypi',):
-            name = name.replace('_', '-')
-        name = quote(name)
+        if namespace:
+            purl.append(namespace)
+            purl.append('/')
+
         purl.append(name)
 
-        if self.version:
+        if version:
             purl.append('@')
-            purl.append(quote(self.version.strip()))
+            purl.append(version)
 
-        if self.qualifiers:
-            quals = {
-                k.strip().lower(): quote(v) for k, v in self.qualifiers.items()
-                if k and k.strip() and v and v.strip()
-            }
-            quals = sorted(quals.items())
-            quals = ['{}={}'.format(k, v) for k, v in quals]
-            quals = '&'.join(quals)
-            if quals:
-                purl.append('?')
-                purl.append(quals)
+        if qualifiers:
+            purl.append('?')
+            purl.append(qualifiers)
 
-        if self.subpath:
+        if subpath:
             purl.append('#')
-            subpaths = self.subpath.split('/')
-            subpaths = [sp for sp in subpaths if sp and sp.strip()
-                        and sp not in ('.', '..')]
-            if subpaths:
-                subpaths = map(quote, subpaths)
-                purl.append('/'.join(subpaths))
+            purl.append(subpath)
 
         return ''.join(purl)
 
     @classmethod
-    def from_string(cls, purl, require_version=False):
+    def from_string(cls, purl):
         """
         Return a PackageURL object parsed from a string.
         Raise ValueError on errors.
-        Optionally raise ValuError if require_version is True and no version
-        is provided.
         """
         if (not purl or not isinstance(purl, basestring)
             or not purl.strip()):
@@ -159,15 +220,6 @@ class PackageURL(namedtuple('PackageURL',
 
         head, sep, subpath = purl.rpartition('#')
         if sep:
-            if subpath:
-                subpaths = subpath.split('/')
-                subpaths = [
-                    sp for sp in subpaths
-                    if sp and sp.strip() and sp not in ('.', '..')
-                ]
-                if subpaths:
-                    subpaths = map(unquote, subpaths)
-                    subpath = '/'.join(subpaths)
             subpath = subpath or None
         else:
             head = subpath
@@ -175,14 +227,6 @@ class PackageURL(namedtuple('PackageURL',
 
         head, sep, qualifiers = head.rpartition('?')
         if sep:
-            if qualifiers:
-                qualifiers = qualifiers.split('&')
-                qualifiers = [kv.partition('=') for kv in qualifiers]
-                qualifiers = {
-                    k.strip().lower(): unquote(v)
-                    for k, _, v in qualifiers
-                    if k and k.strip() and v and v.strip()
-                }
             qualifiers = qualifiers or None
         else:
             head = qualifiers
@@ -190,30 +234,23 @@ class PackageURL(namedtuple('PackageURL',
 
         head, sep, version = head.rpartition('@')
         if sep:
-            if version and version.strip():
-                version = unquote(version).strip()
             version = version or None
         else:
             head = version
             version = None
 
-        if require_version and not version:
+        type, sep, ns_name = head.partition(':')
+        type = type.strip().lower()
+        if not type or not sep:
             raise ValueError(
                 'purl is missing the required '
-                'version component: {}'.format(repr(purl)))
-
-        typ, sep, ns_name = head.partition(':')
-        typ = typ.strip().lower()
-        if not typ or not sep:
-            raise ValueError(
-                'purl is missing the required '
-                'type component: {}'.format(repr(purl)))
+                'type component: {}.'.format(repr(purl)))
 
         ns_name = ns_name.strip().strip('/')
         ns_name = ns_name.split('/')
-        ns_name = [unquote(seg).strip() for seg in ns_name
-                   if seg and seg.strip()]
+        ns_name = [seg for seg in ns_name if seg and seg.strip()]
         namespace = ''
+        name = ''
         if len(ns_name) > 1:
             name = ns_name[-1]
             ns = ns_name[0:-1]
@@ -226,7 +263,9 @@ class PackageURL(namedtuple('PackageURL',
                 'purl is missing the required '
                 'name component: {}'.format(repr(purl)))
 
-        namespace = namespace or None
+        type, namespace, name, version, qualifiers, subpath = normalize(
+            type, namespace, name, version, qualifiers, subpath,
+            encode=False
+        )
 
-
-        return PackageURL(typ, namespace, name, version, qualifiers, subpath)
+        return PackageURL(type, namespace, name, version, qualifiers, subpath)
