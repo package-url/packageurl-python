4a933936387ef8d5d0a7415e18e1682677f2d1a1
Philippe Ombredanne <pombredanne@nexb.com>
2017-11-21 23:16:38 +0100

    Use a simpler package layout
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-from setuptools import find_packages
+from glob import glob
+from os.path import basename
+from os.path import splitext
+
 from setuptools import setup
 
 
@@ -17,8 +20,10 @@ setup(
         'This is a microlibrary implmenting teh purl spec at https://github.com/package-url',
     author='the purl authors',
     url='https://github.com/package-url/packageurl-python',
+    #packages=find_packages('src'),
     package_dir={'': 'src'},
-    packages=find_packages('src'),
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+
     include_package_data=True,
     zip_safe=False,
     platforms='any',
