7890ca0e9c9c78cce69f3c8c3b9766e12d2ea402
Philippe Ombredanne <pombredanne@nexb.com>
2018-11-07 08:04:37 -0800

    Bump version and update setup to new layout #12
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- setup.py
+++ setup.py
@@ -8,21 +8,22 @@ from glob import glob
 from os.path import basename
 from os.path import splitext
 
+from setuptools import find_packages
 from setuptools import setup
+
 
 setup(
     name='packageurl-python',
-    version='0.7.0',
+    version='0.8.0',
     license='MIT',
     description='A "purl" aka. package URL parser and builder',
     long_description='Python library to parse and build "purl" aka. package URLs. '
     'This is a microlibrary implementing the purl spec at https://github.com/package-url',
     author='the purl authors',
     url='https://github.com/package-url/packageurl-python',
-    # packages=find_packages('src'),
+    packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
-
     include_package_data=True,
     zip_safe=False,
     platforms='any',
