d5615312f6cb1f50c2153adaa06e4cb5c3538d2e
Thomas Druez <tdruez@nexb.com>
2020-07-29 10:19:18 +0400

    Fix deprecation warning #36
    Signed-off-by: Thomas Druez <tdruez@nexb.com>

--- src/packageurl/contrib/django_models.py
+++ src/packageurl/contrib/django_models.py
@@ -30,7 +30,7 @@ from __future__ import unicode_literals
 
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from packageurl import PackageURL
 
