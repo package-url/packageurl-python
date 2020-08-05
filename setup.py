e67082be781b9f0ad8bd3c76d14d57dfbb9bb321
Philippe Ombredanne <pombredanne@nexb.com>
2020-08-05 12:15:30 +0200

    Bump version
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 
 setup(
     name='packageurl-python',
-    version='0.9.0',
+    version='0.9.1',
     license='MIT',
     description='A "purl" aka. package URL parser and builder',
     long_description='Python library to parse and build "purl" aka. package URLs. '
