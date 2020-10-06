da265da54896d7e9965e5b7a1018d781925c935e
Thomas Druez <tdruez@nexb.com>
2020-10-06 13:13:35 +0400

    Add a PackageURLFilter class for Django FilterSet implementations #48
    Signed-off-by: Thomas Druez <tdruez@nexb.com>

---
+++ src/packageurl/contrib/django/filters.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) the purl authors
+# SPDX-License-Identifier: MIT
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# Visit https://github.com/package-url/packageurl-python for support and
+# download.
+
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import django_filters
+
+
+class PackageURLFilter(django_filters.CharFilter):
+    """
+    Filter by a Package URL string.
+    Empty values are not applied to the QuerySet.
+
+    This filter depends on a `for_package_url` method available on the Model
+    Manager, see for example `PackageURLQuerySetMixin`.
+    """
+    def filter(self, qs, value):
+        empty_values = ([], (), {}, '', None)
+        if value in empty_values:
+            return qs
+
+        if self.distinct:
+            qs = qs.distinct()
+        return qs.for_package_url(value)
