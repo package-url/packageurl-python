797e20dbaef564c8e2d1a4fbcf8df9a094d5c4cf
Tushar Goel <tushar.goel.dav@gmail.com>
2024-03-07 19:43:27 +0530

    Add support for go packages in manifest files
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>

--- src/packageurl/utils.py
+++ src/packageurl/utils.py
@@ -30,12 +30,17 @@ from packageurl import PackageURL
 def get_golang_purl(go_package: str):
     """
     Return a PackageURL object given an imported ``go_package``
+    >>> get_golang_purl(go_package="github.com/gorilla/mux v1.8.1")
+    PackageURL(type="golang", namespace="github.com/gorilla", name="mux", version="v1.8.1")
     """
     if not go_package:
         return
+    version = None
+    if " " in go_package:
+        go_package , _ , version = go_package.rpartition(" ")    
     parts = go_package.split("/")
     if not parts:
         return
     name = parts[-1]
     namespace = "/".join(parts[:-1])
-    return PackageURL(type="golang", namespace=namespace, name=name)
+    return PackageURL(type="golang", namespace=namespace, name=name, version=version)
