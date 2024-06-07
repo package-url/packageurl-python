f7a88bac736b16234072e16bc0926a1ea946eb68
John M. Horan <johnmhoran@gmail.com>
2024-06-07 09:47:23 -0700

    Restore comments re support/download URL #143
    Reference: https://github.com/package-url/packageurl-python/issues/143
    
    Signed-off-by: John M. Horan <johnmhoran@gmail.com>

--- src/packageurl/contrib/purl2url.py
+++ src/packageurl/contrib/purl2url.py
@@ -21,6 +21,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# Visit https://github.com/package-url/packageurl-python for support and
+# download.
 
 from packageurl import PackageURL
 from packageurl.contrib.route import NoRouteAvailable
--- tests/contrib/test_purl2url.py
+++ tests/contrib/test_purl2url.py
@@ -21,6 +21,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+# Visit https://github.com/package-url/packageurl-python for support and
+# download.
 
 import pytest
 
